e0be9772b2719bb2ac1fa3b4c6539172fc66eef6
Piotr Laskowski <plaskowski@box.com>
2025-07-02 14:15:08 +0200

    Fix import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'database/database.dart';
-import 'screens/home_screen.dart';
+import 'screens/home/home_screen.dart';
 import 'services/attendance_service.dart';
 import 'services/dancer_import_service.dart';
 import 'services/dancer_service.dart';
